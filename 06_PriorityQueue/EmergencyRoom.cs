d5f0299272fa1191a200b21a643e53371ed49f90
YongJoon_Kim <be2163@korea.ac.kr>
2023-04-24 21:48:29 +0900

    feat. Save file

---
+++ 06_PriorityQueue/EmergencyRoom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure 
+{
+    // 0. 응급실 만들기
+    // (골든타임을 입력받아서)
+    // 우선순위가 높은 환자를 먼저 처리하기 위한 EmergencyRoom
+    internal class EmergencyRoom<TName, TPriority>
+    {
+        public EmergencyRoom()
+        {
+
+        }
+    }
+}
