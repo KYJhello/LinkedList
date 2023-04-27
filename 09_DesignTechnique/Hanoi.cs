dd8e0c133d0ed991a0cee10102c45c31a72a23e4
YongJoon_Kim <be2163@korea.ac.kr>
2023-04-27 21:48:50 +0900

    feat. Save DesignTech

---
+++ 09_DesignTechnique/Hanoi.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DesignTechnique
+{
+    internal class Hanoi
+    {//static public void MoveAtoB( Stack<int> A, Stack<int> B)
+     //{
+     //    if (A == null)
+     //        throw new ArgumentNullException();
+     //    B.Push(A.Pop());
+     //}
+     //        public static void Move(int count, int start, int end)
+     //        {
+     //            //if(count > stick[0].Count)
+     //            //{
+     //            //    throw new ArgumentOutOfRangeException();
+     //            //}
+     //            if (count == 1)
+     //            {
+     //                int node = stick[start].Pop();
+     //                stick[end].Push(node);
+     //                Console.WriteLine($"{start} 스틱에서 {end} 스틱으로 {node} 블럭이 이동함");
+     //                return;
+     //            }
+     //            int other = 3 - start - end;
+
+        //            Move(count - 1, start, other);
+        //            Move(1, start, end);
+        //            Move(count - 1, other, end);
+        //        }
+        //        public static Stack<int>[] stick;
+
+        //        //Stack<int> H1 = new Stack<int>();
+        //        //Stack<int> H2 = new Stack<int>();
+        //        //Stack<int> H3 = new Stack<int>();
+
+        //        //H1.Push(4);
+        //        //H1.Push(3);
+        //        //H1.Push(2);
+        //        //H1.Push(1);
+
+        //        //MoveAtoB( H1,  H2);
+
+
+        //        int stackLen = 5;
+
+        //        stick = new Stack<int>[stackLen];
+        //            for(int i  = 0; i<stick.Length; i++)
+        //            {
+        //                stick[i] = new Stack<int>();
+        //            }
+        //            for(int i = stackLen; i > 0 ; i--)
+        //            {
+        //                stick[0].Push(i);
+        //}
+
+        //Move(stackLen, 0, 2);
+
+    }
+}
---
+++ 09_DesignTechnique/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+
+namespace _09_DesignTechnique
+{
+    internal class Program
+    {
+        static StringBuilder? tempStr;
+        // 1. 재귀 하노이탑 문제
+        /*
+        static StringBuilder? printStr;
+        static int count = 0;
+
+        public static void Move(int n, int start, int end, ref Stack<int>[] stick)
+        {
+
+            if (n == 1)
+            {
+                count++;
+                int node = stick[start].Pop();
+                stick[end].Push(node);
+                printStr.Append((start + 1) + " " + (end + 1) + "\n");
+                return;
+            }
+            int other = 3 - start - end;
+
+            Move(n - 1, start, other, ref stick);
+            Move(1, start, end, ref stick);
+            Move(n - 1, other, end, ref stick);
+        }
+        public void Hanoi()
+        {
+            printStr = new StringBuilder("");
+            int n = int.Parse(Console.ReadLine());
+            // 0, 1, 2 스택 생성
+            Stack<int>[] stick = new Stack<int>[3];
+            // 각 스택 선언
+            stick[0] = new Stack<int>();
+            stick[1] = new Stack<int>();
+            stick[2] = new Stack<int>();
+
+            // 처음 스택에 n, n-1, ... , 1 순으로 저장
+            for (int i = n; i > 0; i--)
+            {
+                stick[0].Push(i);
+            }
+
+            Move(n, 0, 2, ref stick);
+            Console.WriteLine(count);
+            Console.WriteLine(printStr.ToString());
+        }
+        */
+        // 2. 백트래킹
+        public static void NandM(int n , int m)
+        {
+            int len = 1;
+            for(int i = 0; i < m; i++)
+            {
+                len *= n;
+            }
+            int[] arr = new int[len];
+
+            
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            tempStr = new StringBuilder("");
+
+            int input = int.Parse(Console.ReadLine().Trim());
+            int n1 = input / 10;
+            int n2 = input % 10;
+            NandM(n1, n2);
+            
+        }
+    }
+}
