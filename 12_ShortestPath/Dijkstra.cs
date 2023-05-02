b79893d41c1addd4195928bdac43f42cc6789036
Kim Yong Joon <be6436@naver.com>
2023-05-02 21:23:28 +0900

    feat.Dijkstra

--- 12_ShortestPath/Dijkstra.cs
+++ 12_ShortestPath/Dijkstra.cs
@@ -4,9 +4,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _12_ShortestPath
+namespace DataStructure
 {
-    internal class Dijkstra
+    internal static class Dijkstra
     {
+        // int.MaxValue는 오버플로우 위험있어 사용하지 말기
+        //              2147483647
+        const int INF = 9999999;
+
+        // 사용할 그래프를 받아오고 distance와 path를 out 참조로 사용
+        public static void ShortestPath(in int[,] graph, int start, out int[] distance, out int[] path )
+        {
+            // 그래프의 y좌표 길이 받기
+            int size = graph.GetLength(0);
+            // 방문했는지 확인을 위한 배열 선언
+            bool[] visited = new bool[size];
+
+            // out 참조로 받아온 매개변수 초기화
+            distance = new int[size];
+            path = new int[size];
+
+            // path와 distance 배열에 값 넣기
+            for (int i = 0; i < size; i++)
+            {
+                distance[i] = graph[start, i];
+                path[i] = graph[start, i] < INF ? start : -1;
+                // i가 시작점이라면 path[i]는 -1로
+                if (i == start)
+                {
+                    path[i] = -1;
+                }
+            }
+
+            ////////////// 탐색 시작 /////////////////
+            // 최대 반복 횟수
+            for (int i = 0; i < size; i++)
+            {
+                // 1. 방문하지 않은 정점 중 가장 가까운 정점부터 탐색
+                // 1-1)경로 및 코스트 초기화
+                int next = -1;
+                int minCost = INF;
+                for (int j = 0; j < size; j++)
+                {
+                    if (!visited[j] &&
+                        distance[j] < minCost)
+                    {
+                        next = j;
+                        minCost = distance[j];
+                    }
+                }
+                // next가 -1이면 탈출
+                if (next < 0)
+                    break;
+
+                // 2. 직접연결된 거리보다 거쳐서 더 짧아진다면 갱신.
+                for (int j = 0; j < size; j++)
+                {
+                    // cost[j] : 목적지까지 직접 연결된 거리
+                    // cost[next] : 탐색중인 정점까지 거리
+                    // graph[next, j] : 탐색중인 정점부터 목적지의 거리
+                    if (distance[j] > distance[next] + graph[next, j])
+                    {
+                        distance[j] = distance[next] + graph[next, j];
+                        path[j] = next;
+                    }
+                }
+                visited[next] = true;
+            }
+        }
+
     }
 }
--- 12_ShortestPath/Program.cs
+++ 12_ShortestPath/Program.cs
@@ -1,28 +1,49 @@
-﻿namespace _12_ShortestPath
+﻿using System.Linq;
+namespace _12_ShortestPath
 {
-    
     internal class Program
     {
-        public static int[,] GetSolution(int[,] arr1, int[,] arr2)
+        private static void PrintDijkstra(int[] distance, int[] path)
         {
-            int[,] answer = new int[arr1.GetLength(0), arr1.GetLength(1)] ;
+            Console.Write("Vertex");
+            Console.Write("\t");
+            Console.Write("dist");
+            Console.Write("\t");
+            Console.WriteLine("path");
 
-            for (int i = 0; i < arr1.GetLength(0); i++)
+            for (int i = 0; i < distance.Length; i++)
             {
-                for(int j = 0;  j < arr1.GetLength(1); j++)
-                {
-                    answer[i, j] = arr1[i, j] + arr2[i, j];
-                }
+                Console.Write("{0,3}", i);
+                Console.Write("\t");
+                if (distance[i] >= INF)
+                    Console.Write("INF");
+                else
+                    Console.Write("{0,3}", distance[i]);
+                Console.Write("\t");
+                if (path[i] < 0)
+                    Console.WriteLine("  X ");
+                else
+                    Console.WriteLine("{0,3}", path[i]);
             }
-
-            return answer;
         }
+
+        const int INF = 9999999;
         static void Main(string[] args)
         {
-            int[,] arr1 = { { 1, 2 }, { 2, 3 } };
-            int[,] arr2 = { { 3, 4 }, { 5, 6 } };
+            int[,] arr1 = new int[8, 8] {{0  ,9  ,9  ,INF,INF,INF,INF,INF},
+                                         {8  ,0  ,6  ,INF,INF,INF,INF,INF},
+                                         {5  ,INF,0  ,INF,6  ,INF,5  ,INF},
+                                         {INF,4  ,INF,0  ,INF,INF,INF,INF},
+                                         {INF,INF,5  ,INF,0  ,INF,2  ,INF},
+                                         {INF,INF,INF,INF,INF,0  ,9  ,5  },
+                                         {INF,INF,INF,INF,INF,INF,0  ,INF},
+                                         {INF,INF,INF,INF,INF,9  ,9  ,0  }};
+            int[] distance;
+            int[] path;
+            DataStructure.Dijkstra.ShortestPath(arr1, 1,out distance, out path);
+            Console.WriteLine("<Dijkstra>");
+            PrintDijkstra(distance, path);
 
-            GetSolution(arr1, arr2);
         }
     }
 }
